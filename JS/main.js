5a44d075311e845d445cbfaeb3aadecb11ca58fd
natali sch <schwartznatali@gmail.com>
2023-05-26 17:18:28 -0300

    javascript agregado

--- JS/main.js
+++ JS/main.js
@@ -106,4 +106,70 @@ const productos = [
         },
         "precio": 1000
     },
-]
+];
+
+//elementos llamados del HTML
+
+const contenedorProductos = document.querySelector("#contenedor-productos");
+const botonesCategorias = document.querySelectorAll(".boton-categoria");
+const tituloPrincipal = document.querySelector("#titulo-principal");
+let botonesAgregar = document.querySelectorAll(".producto-agregar");
+
+console.log(botonesAgregar);
+
+
+
+function cargarProductos(productosElegidos) {
+
+    contenedorProductos.innerHTML = "";
+    //recorrer el array de productos y que los muestre
+    productosElegidos.forEach(producto =>{
+        //creamos el contenedor de cada producto
+        const div = document.createElement("div");
+        div.classList.add("producto");
+        // dentro del div anterior queremos insertar toda la info
+        div.innerHTML = ` 
+            <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+            <div class="producto-detalles">
+                <h3 class="producto-titulo">${producto.titulo}</h3>
+                <p class="producto-precio">${producto.precio}</p>
+                <button class="producto-agregar" id="${producto.id}">Agregar</button>
+            </div>
+        `
+        //insertamos
+        contenedorProductos.append(div);
+
+    })
+};
+
+//cuando cargue la pagina quiero que me muestre todos los productos
+cargarProductos(productos);
+
+//botonCategorias es un array de botones.por cada boton queremos varias cosas 
+botonesCategorias.forEach(boton =>{
+    boton.addEventListener("click", (e)=>{
+        //al darle click al boton se agregue la clase active 
+
+        botonesCategorias.forEach(boton =>{
+            boton.classList.remove("active")
+        });
+        e.currentTarget.classList.add("active");    // aplico current target porque hace referencia al boton + icono, osea que hace referencia al elemento donde aplicamos addeventlistener y no solo el icono. si estaria solo target al dar click en el icono no funciona 
+
+        if(e.currentTarget.id != "todos"){ // si el usuario esta clickeando en los botones que tengan como id , sabanas, mantas o cortinas 
+            const productoCategoria = productos.find(producto => producto.categoria.id === e.currentTarget.id);
+            tituloPrincipal.innerText = productoCategoria.categoria.nombre;
+            const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id)
+            //al dar click en alguna categoria quiero que me cargue especificamente los productos de esa categoria. por eso pongo como condicion que coincidan los ids. 
+            cargarProductos(productosBoton);
+        }else{
+            tituloPrincipal.innerText = "Todos los productos"
+            cargarProductos(productos); // array principal que contiene todos los productos 
+        }
+    })
+
+
+});
+
+function actualizarBotonesAgregar () {
+    
+}
