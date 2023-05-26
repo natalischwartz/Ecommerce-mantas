d892aa0f5ed60eca679e6601c4dca64253349f51
natali sch <schwartznatali@gmail.com>
2023-05-26 14:49:41 -0300

    html y css de la e-commerce

---
+++ JS/main.js
@@ -0,0 +1,109 @@
+//Variables
+console.log("aqui estoy");
+
+
+
+//PRODUCTOS
+
+const productos = [
+    {
+        "id": "sabana-01",
+        "titulo": "Sabana 01",
+        "imagen": "./img/sabanas/sabanas-menuchas.jpeg",
+        "categoria": {
+            "nombre": "Sabanas",
+            "id": "sabanas"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "sabana-02",
+        "titulo": "Sabana 02",
+        "imagen": "./img/sabanas/sabanas-menuchas2.jpeg",
+        "categoria": {
+            "nombre": "Sabanas",
+            "id": "sabanas"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "sabana-03",
+        "titulo": "Sabana 03",
+        "imagen": "./img/sabanas/sabanas-microfibra.png",
+        "categoria": {
+            "nombre": "Sabanas",
+            "id": "sabanas"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "manta-01",
+        "titulo": "Manta 01",
+        "imagen": "./img/mantas/frazada-termica.png",
+        "categoria": {
+            "nombre": "Mantas",
+            "id": "mantas"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "manta-02",
+        "titulo": "Manta 02",
+        "imagen": "./img/mantas/mantas1.png",
+        "categoria": {
+            "nombre": "Mantas",
+            "id": "mantas"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "manta-03",
+        "titulo": "Manta 03",
+        "imagen": "./img/mantas/mantas2.png",
+        "categoria": {
+            "nombre": "Mantas",
+            "id": "mantas"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "manta-04",
+        "titulo": "Manta 04",
+        "imagen": "./img/mantas/mantas3.png",
+        "categoria": {
+            "nombre": "Mantas",
+            "id": "mantas"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "cortina-01",
+        "titulo": "Cortina 01",
+        "imagen": "./img/cortinas/cortina-con-presilla-faldon.png",
+        "categoria": {
+            "nombre": "Cortinas",
+            "id": "cortinas"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "cortina-02",
+        "titulo": "Cortina 02",
+        "imagen": "./img/cortinas/cortina-faldon.jpeg",
+        "categoria": {
+            "nombre": "Cortinas",
+            "id": "cortinas"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "cortina-03",
+        "titulo": "Cortina 03",
+        "imagen": "./img/cortinas/cortina-presillas.png",
+        "categoria": {
+            "nombre": "Cortinas",
+            "id": "cortinas"
+        },
+        "precio": 1000
+    },
+]
